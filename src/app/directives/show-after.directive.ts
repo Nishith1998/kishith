f38abb09e8b889b512b146249990a7daef40390a
Nishith Goswami <nishithgoswami23@gmail.com>
2023-10-11 13:22:22 +0000

    show after direction animation addition

--- src/app/directives/show-after.directive.ts
+++ src/app/directives/show-after.directive.ts
@@ -1,10 +1,13 @@
 import { Directive, Input, ElementRef, Renderer2, OnInit, HostBinding } from '@angular/core';
+import gsap from "gsap";
+
 
 @Directive({
   selector: '[showAfter]'
 })
 export class ShowAfterDirective implements OnInit {
   @Input('showAfter') delay!: number; // Input for specifying the delay in milliseconds
+  @Input() animations!: string; // Input for specifying the delay in milliseconds
 
   @HostBinding('style.visibility') display = 'hidden';
   constructor(private el: ElementRef, private renderer: Renderer2) {}
@@ -14,6 +17,47 @@ export class ShowAfterDirective implements OnInit {
       setTimeout(() => {
         // this.renderer.removeStyle(this.el.nativeElement, 'display');
         this.display = 'visible';
+        if(this.animations) {
+          this.applyAnimations(this.animations);
+        }
       }, this.delay);
+  }
+
+  applyAnimations(animationType: string) {
+    switch(animationType) {
+      case 'fadeInLeftToRight':
+          gsap.fromTo(
+              this.el.nativeElement,
+              {
+                x: -100,
+                opacity: 0,
+              },
+              {
+                x: 0,
+                opacity: 1,
+                stagger: 0.05,
+                duration: 2,
+                ease: 'power4.out',
+              }
+            );
+            break;
+            
+      case 'fadeInRightToLeft':
+          gsap.fromTo(
+              this.el.nativeElement,
+              {
+                x: 100,
+                opacity: 0,
+              },
+              {
+                x: 0,
+                opacity: 1,
+                stagger: 0.05,
+                duration: 2,
+                ease: 'power4.out',
+              }
+          );
+          break;
+  }
   }
 }
